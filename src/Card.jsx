6b600711081a3ec923ff837ed65b418545245556
Hamed Jahan <iamhamedjahan@gmail.com>
2024-08-08 17:09:42 +0330

    version 1

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,28 +1,50 @@
 import styles from "./Card.module.css";
 import logo from "../public/images/logo.png";
-import eitaa from "../public/images/eitaa-icon-white.svg";
-import { CiMobile3 } from "react-icons/ci";
-import { FaWhatsapp } from "react-icons/fa";
+import eitaa from "../public/images/eitaa-icon-black.svg";
+import { IoCallSharp } from "react-icons/io5";
+import { FaTelegramPlane } from "react-icons/fa";
+import { RiWhatsappFill } from "react-icons/ri";
+
+const about = {
+  education: `دانش آموخته پردیس دانشگاه تهران با معدل 20`,
+  experience: `6 سال سابقه فعالیت حقوقی حین تحصیل`,
+  skills: `مسلط به روند پیگیری انواع پرونده های قضایی و کیفری`,
+};
 
 function Card() {
   return (
     <div className={styles.card}>
-      <img className={styles.logoBar} src={logo} />
-      <div className={styles.name}>
-        <div>زینب جهانگیری</div>
-        <small>کارآموز کانون وکلای مرکز</small>
-        <span>شماره پروانه: 38269</span>
+      <div className={styles.header}>
+        <img className={styles.logo} src={logo} />
+        <div className={styles.name}>
+          <div>زینب جهانگیری</div>
+          <small>کارآموز کانون وکلای مرکز</small>
+          <span>ش.پ: 38269</span>
+        </div>
       </div>
-      <div className={styles.actionBar}>
-        <img src={eitaa} />
-        <a href="https://wa.me/+989129215791"></a>
-        <FaWhatsapp className={styles.icon} />
-        <a href="tel:+989129215791">
-          <CiMobile3 className={styles.icon} />
-        </a>
+
+      <div className={styles.main}>
+        <h4>ارتباط با من</h4>
+        <div>
+          <a href="tel:+989129215791">
+            <IoCallSharp className={styles.icon} />
+          </a>
+          <a href="https://t.me/Zeynabjahangiri">
+            <FaTelegramPlane className={styles.icon} />
+          </a>
+          <a href="https://wa.me/+989129215791">
+            <RiWhatsappFill className={styles.icon} />
+          </a>
+          <img src={eitaa} className={styles.icon} />
+        </div>
       </div>
     </div>
   );
 }
 
 export default Card;
+
+{
+  /* <div className={styles.actionBar}>
+      </div> */
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Landing from "./Landing";
-import "./index.css";
+import Card from "./Card";
+import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Landing />
+    <Card/>
   </React.StrictMode>
 );
